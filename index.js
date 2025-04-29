fb4b1f6342b139d4918d8e7d86e9c93dfbf1ce1f
Zara Cosmin <87174817+tinulica@users.noreply.github.com>
2025-04-29 21:36:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
+// index.js
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const authRoutes = require('./routes/auth');
-const inviteRoutes = require('./routes/invite');
-const entryRoutes = require('./routes/entries');
-const app = express();
+const { PrismaClient } = require('@prisma/client');
+
+const authRoutes     = require('./routes/auth');
+const inviteRoutes   = require('./routes/invite');
+const entryRoutes    = require('./routes/entries');
+const passwordRoutes = require('./routes/password');
+
+const app    = express();
+const prisma = new PrismaClient();
 
 app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 
 // Health check
-app.get('/', (req, res) => res.send('Glovo HR Backend is running'));
+app.get('/', (req, res) => {
+  res.send('Glovo HR Backend is running');
+});
 
 // Mount routers
 app.use('/auth', authRoutes);
 app.use('/invite', inviteRoutes);
 app.use('/entries', entryRoutes);
+app.use('/password', passwordRoutes);
 
 // Global error handler
 app.use((err, req, res, next) => {
@@ -25,4 +34,6 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
