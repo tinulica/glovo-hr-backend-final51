f6177f3a284009da6f31b208a8f9a3a49a3898f1
Zara Cosmin <87174817+tinulica@users.noreply.github.com>
2025-04-30 13:37:27 +0300

    Create report.js

---
+++ routes/report.js
@@ -0,0 +1,10 @@
+// src/routes/report.js
+import express from "express";
+const router = express.Router();
+
+// Placeholder route
+router.get("/", (req, res) => {
+  res.send("Report route is working!");
+});
+
+export default router;
