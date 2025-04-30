399f1b37173356410b5908bced63f0982cb8dc0a
Zara Cosmin <87174817+tinulica@users.noreply.github.com>
2025-04-30 15:13:32 +0300

    Create parseXLSX.js

---
+++ utils/parseXLSX.js
@@ -0,0 +1,10 @@
+// src/utils/parseXLSX.js
+import XLSX from 'xlsx';
+
+export function parseXLSX(fileBuffer) {
+  const workbook = XLSX.read(fileBuffer, { type: 'buffer' });
+  const sheetName = workbook.SheetNames[0];
+  const sheet = workbook.Sheets[sheetName];
+  const rows = XLSX.utils.sheet_to_json(sheet, { defval: '' });
+  return rows;
+}
