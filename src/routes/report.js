fedbd7d35efa1ba0cc93a727a33c0f8095cfe21c
Zara Cosmin <87174817+tinulica@users.noreply.github.com>
2025-04-30 13:34:23 +0300

    Delete src/routes directory

--- src/routes/report.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const router = express.Router();
-
-// Example report route
-router.get('/', (req, res) => {
-  res.send('Report route working!');
-});
-
-export default router;
