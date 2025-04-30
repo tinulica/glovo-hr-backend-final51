6c275a64b5cc22ead4987824a9726e6232ff3077
Zara Cosmin <87174817+tinulica@users.noreply.github.com>
2025-04-30 12:17:01 +0300

    Create upload.js

---
+++ middleware/upload.js
@@ -0,0 +1,24 @@
+// middleware/upload.js
+import multer from "multer";
+import path from "path";
+import { fileURLToPath } from "url";
+import fs from "fs";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const uploadPath = path.join(__dirname, "../uploads");
+    if (!fs.existsSync(uploadPath)) fs.mkdirSync(uploadPath);
+    cb(null, uploadPath);
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix + "-" + file.originalname);
+  }
+});
+
+const upload = multer({ storage });
+
+export default upload;
